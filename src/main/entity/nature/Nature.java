a1cdd2dce39a62dd234922c01e279df52c13fb05
Ksenia Ostasheva <ksenia.ostasheva@gmail.com>
2024-12-06 16:48:32 +0100

    clean Nature

--- src/main/entity/nature/Nature.java
+++ src/main/entity/nature/Nature.java
@@ -1,15 +1,4 @@
 package main.entity.nature;
 
 public abstract class Nature {
-
-//    private double weight;
-
-//    public void setWeight(double newWeight) {
-//        weight = newWeight;
-//    }
-//
-//    public double getWeight() {
-//        return weight;
-//    }
-
 }
